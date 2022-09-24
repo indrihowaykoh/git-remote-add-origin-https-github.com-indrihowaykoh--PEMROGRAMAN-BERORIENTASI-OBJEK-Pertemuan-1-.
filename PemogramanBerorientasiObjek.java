13bc7ff5e7853e29116cba151c9666110d185fd5
Obed Edom Howaykoh <103534732+indrihowaykoh@users.noreply.github.com>
2022-09-24 14:54:09 +0700

    Add files via upload

---
+++ PemogramanBerorientasiObjek.java
@@ -0,0 +1,18 @@
+
+package kelas.belajarjava;
+
+
+public class PemogramanBerorientasiObjek {
+  private static boolean JAVA;
+
+    
+    public static void main(String[] args) {
+        
+        String Pogram = " Sangat mudah ";
+     
+    System.out.println("Belajar JAVA ");
+    
+    System.out.println("Sanggat mudah sekali");
+    System.out.println("dan Sanggat Menyenangkan...");
+    }
+}
